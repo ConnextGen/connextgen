715ae5b024d1c00db3afb1fec5562e42f1894275
armaanpriyadarshan <armaan_priyadarshan@outlook.com>
2024-07-18 15:27:15 -0400

    doogi

---
+++ client/src/pages/Login.js
@@ -0,0 +1,20 @@
+import '../App.css';
+import TextEntry from '../components/TextEntry';
+
+function Login() {
+    return (
+        <div className="background">
+            <div className="pane">
+                <h1>Welcome Back 👋</h1>
+                <TextEntry header="Email"/>
+                <TextEntry header="Password"/>
+                <div className="button-container">
+                    <a href="#" class="button">Login</a>
+                    <h2>Don't have an account? <a href='signup' class="link">Sign Up</a></h2>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
