9196e145b7198797f123e21c67ceebe84fc0597f
armaanpriyadarshan <armaan_priyadarshan@outlook.com>
2024-09-19 00:10:41 -0400

    small tings

--- client/src/components/unit/LatestUnit.jsx
+++ client/src/components/unit/LatestUnit.jsx
@@ -1,12 +1,13 @@
 import { HashLink } from 'react-router-hash-link';
+import { formatTitle } from '../../utils/formattingUtils';
 import styles from './LatestUnit.module.css';
 
 const LatestUnit = ({ unitNumber, unitName, lessonNumber, lessonName, percentage }) => { 
     return (
         <div className={styles.unit}>
             <div className={styles.body}>
-                <h1>Unit {unitNumber}: {unitName}</h1>
-                <h3>Lesson {lessonNumber}: {lessonName}</h3>
+                <h1>Unit {unitNumber}: {formatTitle(unitName)}</h1>
+                <h3>Lesson {lessonNumber}: {formatTitle(lessonName)}</h3>
             </div>
             <div className={styles.bottom}>
                 <div className={styles.progress}>
