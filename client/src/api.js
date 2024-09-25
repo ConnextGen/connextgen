38e8b1cf782ce963f4dac91c4b45c40328765095
armaanpriyadarshan <armaan_priyadarshan@outlook.com>
2024-09-25 10:38:11 -0400

    fixed api url

--- client/src/api.js
+++ client/src/api.js
@@ -1,43 +1,43 @@
 import axios from 'axios';
 
-const ip = 'http://localhost:3001';
+const url = 'https://connextgen-server.vercel.app/';
 
 export async function logIn(email, password) {
-    return (await axios.post(`${ip}/api/users/login`, { email, password })).data;
+    return (await axios.post(`${url}/api/users/login`, { email, password })).data;
 }
 
 export async function signUp(firstName, lastName, school, email, password) {
-    return (await axios.post(`${ip}/api/users/signup`, { firstName, lastName, school, email, password })).data;
+    return (await axios.post(`${url}/api/users/signup`, { firstName, lastName, school, email, password })).data;
 }
 
 export async function getUser(id) {
-    return (await axios.get(`${ip}/api/users/${id}`).then(res => res.data));
+    return (await axios.get(`${url}/api/users/${id}`).then(res => res.data));
 }
 
 export async function getProgress(id) {
-    return (await axios.get(`${ip}/api/users/${id}/progress`)).data;
+    return (await axios.get(`${url}/api/users/${id}/progress`)).data;
 }
 
 export async function completeLesson(unit, lesson, id) {
-    return (await axios.post(`${ip}/api/users/${unit}/${lesson}`, { userId: id })).data;
+    return (await axios.post(`${url}/api/users/${unit}/${lesson}`, { userId: id })).data;
 }
 
 export async function getCourse() {
-    return (await axios.get(`${ip}/api/course`)).data;
+    return (await axios.get(`${url}/api/course`)).data;
 }
 
 export async function getUnit(unit) {
-    return (await axios.get(`${ip}/api/course/${unit}`)).data;
+    return (await axios.get(`${url}/api/course/${unit}`)).data;
 }
 
 export async function getLesson(unit, lesson) {
-    return (await axios.get(`${ip}/api/course/${unit}/${lesson}`)).data;
+    return (await axios.get(`${url}/api/course/${unit}/${lesson}`)).data;
 }
 
 export async function getPreviousLesson(unit, lesson) {
-    return (await axios.get(`${ip}/api/course/${unit}/${lesson}/previous`)).data;
+    return (await axios.get(`${url}/api/course/${unit}/${lesson}/previous`)).data;
 }
 
 export async function getNextLesson(unit, lesson) {
-    return (await axios.get(`${ip}/api/course/${unit}/${lesson}/next`)).data;
+    return (await axios.get(`${url}/api/course/${unit}/${lesson}/next`)).data;
 }
